d205cf2f38d4ac37b9903db75de0aa428c18e3b4
Loic Bouleux <loic.bouleux@gmail.com>
2025-06-27 01:01:10 +0000

    améliore le site avec une police Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased min-h-screen w-full bg-background text-foreground flex')}>
         <SidebarProvider defaultOpen>
