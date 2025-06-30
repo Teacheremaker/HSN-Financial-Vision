6bfa74cd4dafc3b56490e655722d4c5e60710a9e
Loic Bouleux <loic.bouleux@gmail.com>
2025-06-30 15:28:44 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,24 @@
 
-// This directory is for Firebase Functions, which are not used in this project
-// for Netlify deployment. This file is kept to maintain directory structure
-// but the code has been removed to prevent deployment errors.
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {setGlobalOptions} from "firebase-functions/v2";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+// For cost control and performance, set global options.
+setGlobalOptions({maxInstances: 10});
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
